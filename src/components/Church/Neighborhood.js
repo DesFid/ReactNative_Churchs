d5cccf1d3d317ea957218fdc91c14b98d2c803c1
DesFid <desfid@localhost.localdomain>
2018-08-10 06:42:42 -0500

    ALL!

---
+++ src/components/Church/Neighborhood.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+
+class Neighborhoods extends Component {
+    render (){
+        return (
+            <Text>Neighborhoods</Text>
+        )    
+    }
+}
+
+export default Neighborhoods
