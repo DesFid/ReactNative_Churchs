20d1535e4e00b8e879eb1ec425f7c859ef749d9c
DesFid <35884517+DesFid@users.noreply.github.com>
2018-08-22 15:47:54 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,64 +1,48 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ *
+ * @format
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet
-} from 'react-native';
-import List from './src/components/Church/List';
-import Header from './src/components/Header';
-import TabsFooter from './src/components/Footer';
-import { connect } from 'react-redux';
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      isOpen: false,
-      itemSelected: 'Home'
-    }
-    this.itemSelected = this.itemSelected.bind(this)
-  }
-  static navigationOptions = {
-    header: null
-  }
-  
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
-  }
-  itemSelected(item) {
-    this.setState({
-      itemSelected: item,
-      isOpen: false
-    })
-  } paddingTop
-  updateMenu(isOpen) {
-    this.setState({ isOpen })
-  }
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import MapTest from './src/components/MyMap';
 
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Header navigation={this.props.navigation} toggle={this.toggle.bind(this)} />
-        <List
-          navigation={this.props.navigation}
-        />
+        <MapTest />
       </View>
-    )
+    );
   }
 }
-
-export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-  }
-})
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import index from './src/index';
+import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => index);
+AppRegistry.registerComponent(appName, () => App);
